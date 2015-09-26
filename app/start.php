dfaf45bdca9b7b0763e978bc38726b4f3e98a0df
Timothé Mermet-Buffet <mermetbt@gmail.com>
2015-09-26 14:46:14 +0800

    Add the directory for collections.

--- app/start.php
+++ app/start.php
@@ -8,7 +8,8 @@ Biome\Biome::registerDirs(array(
 	'controllers'	=> __DIR__ . '/controllers/',
 	'models'		=> __DIR__ . '/models/',
 	'views'			=> __DIR__ . '/views/',
-	'components'	=> __DIR__ . '/components/'
+	'components'	=> __DIR__ . '/components/',
+	'collections'	=> __DIR__ . '/collections/',
 ));
 
 Biome\Biome::registerAlias(array(
